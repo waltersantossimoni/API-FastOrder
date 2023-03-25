237f585539c86ac9a89dcbcc5388ee47e0259bc1
Walter dos Santos Simoni <waltersantossimoni@gmail.com>
2023-03-24 22:54:55 -0300

    feat: criado as configs iniciais da API sendo elas: configs de ambiente, servidor e armazenamento de dados.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    app.listen(3001, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => (console.log('Erro ao conectar no banco de dados.')));
